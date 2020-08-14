d62cf4e8514c560b063a395f49d30f870a226be6
Josef Fürlinger <josef.fuerlinger@gmail.com>
2020-08-14 11:51:58 +0200

    Added AddOrJoinTeam command

--- MyTeams.UI/ViewModels/TeamsOverviewViewModel.cs
+++ MyTeams.UI/ViewModels/TeamsOverviewViewModel.cs
@@ -1,14 +1,18 @@
-﻿using MyTeams.Core.Contracts;
+﻿using ActReport.ViewModel;
+using MyTeams.Core.Contracts;
 using MyTeams.UI.ViewModels;
 using MyTeams.WpfApp.Infrastructure;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Windows.Input;
 
 namespace MyTeams.WpfApp.ViewModels
 {
   public class TeamsOverviewViewModel : ViewModelBase
   {
     private ChannelViewModel _currentChannel;
+    
 
     public ObservableCollection<ChannelViewModel> Channels { get; set; }
 
@@ -23,8 +27,14 @@ namespace MyTeams.WpfApp.ViewModels
       }
     }
 
+    public ICommand AddOrJoinTeamCommand { get; private set; }
+
     public TeamsOverviewViewModel(ITeamsService teamsService) : base(teamsService)
     {
+      AddOrJoinTeamCommand = new RelayCommand(
+        execute: (_) => Debug.WriteLine("TODO: Add or Join Team"),
+        canExecute: (_) => false);
+
       Channels = new ObservableCollection<ChannelViewModel>();
 
       TeamsService
@@ -35,5 +45,7 @@ namespace MyTeams.WpfApp.ViewModels
 
       CurrentChannel = Channels.FirstOrDefault();
     }
+
+
   }
 }
